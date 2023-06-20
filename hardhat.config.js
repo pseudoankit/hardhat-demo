3a6c55f4c2f761591ceb88556a822e7eb834aa73
ankitkumar <lostankit7@gmail.com>
2023-06-20 07:37:24 +0530

    deployed contract to mumbai test net

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,52 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
+require("@nomiclabs/hardhat-etherscan");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
-};
+
+  solidity: "0.8.6",
+  // settings:{
+  //   optimizer:{
+  //     enabled:true,
+  //     runs:200
+  //   }
+  
+  networks:{
+    hardhat:{
+      chainId:1337
+    },
+    mumbai:{
+      url:`https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API}`,
+      accounts:[process.env.MAIN_ACCOUNT],
+      chainId:80001,
+      url:"https://matic-mumbai.chainstacklabs.com",
+      gas: 300000000000,
+    },
+    // goerli:{
+    //   url: `https://goerli.infura.io/v3/${process.env.INFURA_API}`,
+    //   accounts:[process.env.MAIN_ACCOUNT],
+    //   chainId: 5,
+    // },
+    // mumbai:{
+    //   url:`https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API}`,
+    //   accounts:[process.env.MAIN_ACCOUNT],
+    //   chainId:80001,
+    // },
+ 
+    //go to metamask and select mumbai testnet
+    //go to account details from the three dots option
+    //select export private key
+    //copy and paste pvt key .env Main_account 
+
+  },
+
+  gasReporter:{
+    enabled:true,
+    currency:"INR",
+    coinmarketcap:process.env.COINMARKETCAP,
+    token:"matic",
+    outputFile:"gasReports.txt",
+    noColors:true
+  }
+}
+  
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,29 +1,18 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const{ethers} =require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const RewardToken = await ethers.getContractFactory("RewardToken");
+  const rewardToken = await RewardToken.deploy();
+  await rewardToken.deployed();
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  console.log("RewardToken contract deployed to:", rewardToken.address);
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const Staking = await ethers.getContractFactory("staking");
+  const staking = await Staking.deploy(rewardToken.address, rewardToken.address);
+  await staking.deployed();
 
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log("Staking contract deployed to:", staking.address);
 }
-
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
